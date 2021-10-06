5980a7fd8231e3bab5b736b5f499a6165dd13ee4
TCristianB <ariwachi@gmail.com>
2021-10-05 22:58:52 -0300

    Solving bugs

--- index.js
+++ index.js
@@ -17,6 +17,9 @@ app.use('/users', userRouter)
 
 if (process.env.NODE_ENV === 'production'){
     app.use(express.static('client/build'))
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname,  "build", "index.html"));
+      });
 }
 
 app.listen(PORT, () => console.log(`Server is at ${PORT}`))
